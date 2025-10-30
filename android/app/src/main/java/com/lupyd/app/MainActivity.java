0fc3d5e492e3808d7b177b963b62de415866097b
ash-hashtag <hashtag438@gmail.com>
2025-10-30 11:48:06 +0530

    kotlin

--- android/app/src/main/java/com/lupyd/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.lupyd.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ android/app/src/main/java/com/lupyd/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lupyd.app
+
+import com.getcapacitor.BridgeActivity
+
+class MainActivity : BridgeActivity()
