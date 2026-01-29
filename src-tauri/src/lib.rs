95d847de98108fff1ed389467a6d9a2aa2a620e4
ash-hashtag <hashtag438@gmail.com>
2026-01-29 17:46:46 +0530

    merged upstream

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,8 @@
+use tauri::tray::{TrayIconBuilder, TrayIconEvent};
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    use tauri::Manager
-
-
     tauri::Builder::default()
         .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
             if let Some(w) = app.get_webview_window("main") {
@@ -17,6 +17,29 @@ pub fn run() {
                         .build(),
                 )?;
             }
+
+            let _tray = TrayIconBuilder::with_id("main")
+                .icon(app.default_window_icon().unwrap().clone())
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click { .. } = event {
+                        let app = tray.app_handle();
+
+                        #[cfg(not(target_os = "macos"))]
+                        {
+                            if let Some(webview_window) = app.get_webview_window("main") {
+                                let _ = webview_window.show();
+                                let _ = webview_window.set_focus();
+                            }
+                        }
+
+                        #[cfg(target_os = "macos")]
+                        {
+                            tauri::AppHandle::show(&app.app_handle()).unwrap();
+                        }
+                    }
+                })
+                .build(app)?;
+
             Ok(())
         })
         .run(tauri::generate_context!())
